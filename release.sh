c42474f65d737d5aa4d629e7b466909d99997dd6
akoenig <andreas.koenig@berg-turbenthal.ch>
2024-01-12 13:42:11 +0100

    add release script

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git status -s | grep ^ && exit 1
+
+cargo install cargo-release cargo-get set-cargo-version
+
+cargo-release release "${RELEASE_TYPE=patch}" -x --no-confirm || exit 1
+
+cargo release version patch -x --no-confirm --no-push
+VERSION=$(cargo get package.version)
+set-cargo-version ./Cargo.toml "${VERSION}"-SNAPSHOT
+git add ./Cargo.toml Cargo.lock
+git commit -m "prepare for further development"
+git push
